0e3fb39c3a7783fc379559421649ceaafe9a4247
Sahara <sahara.tm65@gmail.com>
2023-10-12 00:28:05 -0400

    Completed Workbook Exercises

--- MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -4,34 +4,43 @@ public class MathApp {
     public static void main (String[] args){
 
         // Question 1:
-        // declare variables here
-        // then code solution
-        // then use System.out.println() to display results
-        // ex: System.out.println("The answer is " + answer);
-        // REPEAT FOR NEXT EXERCISE
-
         int bobSalary = 10_000;
         int garySalary = 20_000;
         int highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("1. The highest salary is: " + highestSalary);
 
         // Question 2:
         int carPrice = 5_000;
         int truckPrice = 50_000;
-        int smallestPrice = Math.min(carPrice, truckPrice);
+        int smallestVehiclePrice = Math.min(carPrice, truckPrice);
+        System.out.println("2. Smallest price is: " + smallestVehiclePrice);
 
         // Question 3:
-        // Find and display the area of a circle whose radius is 7.25
         double area = 7.25 * 7.25 * Math.PI;
+        System.out.println("3. The radius is: " + area);
 
-        //Question 4:
-        double x = Math.sqrt(9);
+        // Question 4:
+        double numberToBeSquared = 5.0;
+        double squareRootResult = Math.sqrt(numberToBeSquared);
+        System.out.println("4. Square root result: " + squareRootResult);
 
+        // Question 5:
+        int x1 = 5;
+        int y1 = 10;
+        int x2 = 85;
+        int y2 = 50;
+        double distanceBetweenPoints = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("The distance between is: " + distanceBetweenPoints);
 
+        // Question 6:
+        double toBeAbsolute = -3.5;
+        double absoluteNumberResult = Math.abs(toBeAbsolute);
+        System.out.println("The absolute value is: " + absoluteNumberResult);
 
-        System.out.println("The highest salary is " + highestSalary);
-        System.out.println("Smallest price is " + smallestPrice);
-        System.out.println("The radius " + area);
-        System.out.println(x);
+        // Question 7:
+        double randomNumber = Math.random();
+        System.out.println("Your random number is: " + randomNumber);
+
     }
 }
 
--- RentalCarCalculator/src/main/java/com/pluralsight/RentalCarCalculator.java
+++ RentalCarCalculator/src/main/java/com/pluralsight/RentalCarCalculator.java
@@ -22,19 +22,45 @@ public class RentalCarCalculator {
         int rentalDays = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("Electronic Toll: $3.95 per day? (y/n)");
-        boolean hasETag = sc.nextBoolean();
+        System.out.println("Electronic Toll: $3.95 per day? (yes/no)");
+        String hasET = sc.next().toLowerCase();
         sc.nextLine();
 
-        System.out.println("Add GPS: $2.85 per day? (y/n)");
-        boolean hasGPS = sc.nextBoolean();
+        System.out.println("Add GPS: $2.85 per day? (yes/no)");
+        String hasGPS = sc.next().toLowerCase();
         sc.nextLine();
 
-        System.out.println("Roadside Assistance: $3.95 per day? (y/n)");
-        boolean roadsideAssistance = sc.nextBoolean();
+        System.out.println("Roadside Assistance: $3.95 per day? (yes/no)");
+        String roadsideAssistance = sc.next().toLowerCase();
         sc.nextLine();
 
-        double priceOfBasicCarRental = rentalDays * rentalPrice;
+
+        double underageSurcharge = 0;
+        if (age < 25);{
+            underageSurcharge = .3 * rentalPrice;
+        }
+
+        double additionalCosts = 0;
+        if (hasET.equals("yes")) {
+            additionalCosts += 3.95 * rentalDays;
+        }
+
+        if (hasGPS.equals("yes")){
+            additionalCosts += 2.95 * rentalDays;
+        }
+
+        if (roadsideAssistance.equals("yes")){
+            additionalCosts += 3.95 * rentalDays;
+        }
+
+        double totalCost = rentalPrice + additionalCosts + underageSurcharge;
+
+        System.out.println("Standard rental cost: $" + rentalPrice);
+        System.out.println("Additional costs: $" + additionalCosts);
+        if (age > 25) {
+            System.out.println("Underage surcharge: $" + underageSurcharge);
+        }
+        System.out.println("Total Cost: $" + totalCost);
     }
 
 }
